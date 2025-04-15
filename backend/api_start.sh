0186434fb226aa176080905e926f9ce5e412483a
Dylan Vidal <dylanvidal1204@gmail.com>
2025-04-15 17:04:46 -0400

    PLEASE

--- backend/api_start.sh
+++ backend/api_start.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-# Install Python dependencies
+echo "🔍 Checking libGL.so.1"
+find /nix -name "libGL.so.1" || echo "libGL.so.1 not found"
+
+# Python deps
 pip install -r requirements.txt
 
-# Start the app
+# Launch app
 uvicorn main:app --host 0.0.0.0 --port 8000 --reload
