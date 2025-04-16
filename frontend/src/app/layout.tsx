38261dcecf5225af25ece9e57f37500806f31a94
Dylan Vidal <dylanvidal1204@gmail.com>
2025-04-15 21:24:22 -0400

    metadata goat

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -4,33 +4,54 @@ import WebAudioFontLoader from "./components/WebAudioFontLoader";
 import "./globals.css";
 
 const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
+    variable: "--font-geist-sans",
+    subsets: ["latin"],
 });
 
 const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+    variable: "--font-geist-mono",
+    subsets: ["latin"],
 });
 
 export const metadata: Metadata = {
-  title: "AirJam",
-  description: "A one man band, anytime, anywhere.",
+    title: "AirJam – One Man Band",
+    description:
+        "Be a one man band, anytime, anywhere. Play up to 30 instruments with nothing but your hands and a web cam.",
+    themeColor: "#43AA8B",
+    openGraph: {
+        title: "AirJam – One Man Band",
+        description:
+            "Be a one man band, anytime, anywhere. Play up to 30 instruments with nothing but your hands and a web cam.",
+        url: "https://play.airjam.studio",
+        images: [
+            {
+                url: "/media-logo.png",
+                alt: "AirJam Logo",
+            },
+        ],
+    },
+    twitter: {
+        card: "summary_large_image",
+        title: "AirJam – Your All-In-One Musical Companion",
+        description:
+            "Be a one man band, anytime, anywhere. Play up to 30 instruments with nothing but your hands and a web cam.",
+        images: "/media-logo.png",
+    },
 };
 
 export default function RootLayout({
-  children,
+    children,
 }: Readonly<{
-  children: React.ReactNode;
+    children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <WebAudioFontLoader />
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
-    </html>
-  );
+    return (
+        <html lang="en">
+            <WebAudioFontLoader />
+            <body
+                className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+            >
+                {children}
+            </body>
+        </html>
+    );
 }
